929b42df1f331b0b1dfd75e90f04b1a438f9c224
Chinedu <chinedueayinotu@gmail.com>
2021-03-06 16:23:25 -0600

    Updating

--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -4,8 +4,17 @@ import acm.program.ConsoleProgram;
 
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
-	public void run() {
+
+		private String removeAllOccurrences(String str, char ch) {
+			String result = "";
+			for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ch) {
+			result += str.charAt(i);
+			}
+			}
+			return result;
+			}
 
 	}
 	
-}
+
