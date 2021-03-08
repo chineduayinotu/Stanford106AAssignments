2be3eeff99e42cbb3b4bb5ce918b2a3edb2bb573
Chinedu <chinedueayinotu@gmail.com>
2021-03-07 20:56:32 -0600

    update

--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -3,7 +3,16 @@ package week3;
 import acm.program.ConsoleProgram;
 
 public class DeleteCharactersFromString extends ConsoleProgram {
-	
+	public void run() {
+		println(removeAllOccurrences("Hello Sam, how is it going?", 's') );
+		println(removeAllOccurrences("Java is complex and interesting", 'e') );
+		println(removeAllOccurrences("Summer is here!", 'e') );
+		
+		// Removing dashes from mobile numbers
+		
+		println(removeAllOccurrences("346-400-7675", '-') );
+		
+	}
 
 		private String removeAllOccurrences(String str, char ch) {
 			String result = "";
@@ -13,7 +22,7 @@ public class DeleteCharactersFromString extends ConsoleProgram {
 			}
 			}
 			return result;
-			}
+		}
 
 	}
 	
